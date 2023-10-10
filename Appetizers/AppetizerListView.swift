b6b10da9c1215ddbe1ac0248b74fb486d04e85ab
Vicgri <89450050+Vicgri@users.noreply.github.com>
2023-10-10 13:38:14 +0200

    Tabview

---
+++ Appetizers/AppetizerListView.swift
@@ -0,0 +1,18 @@
+//
+//  AppetizerListView.swift
+//  Appetizers
+//
+//  Created by Victoria Grimen on 10/10/2023.
+//
+
+import SwiftUI
+
+struct AppetizerListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppetizerListView()
+}
