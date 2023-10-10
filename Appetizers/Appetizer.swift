006ac2b724c37bee0856738bb8fd9968ccc27f21
Vicgri <89450050+Vicgri@users.noreply.github.com>
2023-10-10 14:31:47 +0200

    DataModel and MockData

--- Appetizers/Appetizer.swift
+++ Appetizers/Appetizer.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+
+//Using decodable since we only retrive and receiving data.
+//If we were going to manipulating the data we would need encodable
+struct Appetizer: Decodable {
+  let id: Int
+  let name: String
+  let description: String
+  let price: Double
+  let imageURL: String
+  let calories: Int
+  let protein: Int
+  let carbs: Int
+}
+
+// Helper response to handle the nested Json
+struct AppetizerResponse {
+  let request: [Appetizer]
+  
+}
+
+struct MockData {
+  
+  static let sampleAppetizer = Appetizer(id: 0001,
+                                         name: "Test Appetizer",
+                                         description: "This is the description for my appetizer",
+                                         price: 9.99,
+                                         imageURL: "",
+                                         calories: 99,
+                                         protein: 99,
+                                         carbs: 99)
+  
+}
+
+
