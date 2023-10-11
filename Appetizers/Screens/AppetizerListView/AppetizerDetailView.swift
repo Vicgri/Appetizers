b3e6830025d8c3935c11033fe9484b64fdc59ffc
Vicgri <89450050+Vicgri@users.noreply.github.com>
2023-10-11 17:31:04 +0200

    Added detailView and styled the card

---
+++ Appetizers/Screens/AppetizerListView/AppetizerDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  AppetizerDetailView.swift
+//  Appetizers
+//
+//  Created by Victoria Grimen on 11/10/2023.
+//
+
+import SwiftUI
+
+struct AppetizerDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppetizerDetailView()
+}
