27962a24667a4460dc8c2125135f562d6161c303
Vicgri <89450050+Vicgri@users.noreply.github.com>
2023-10-12 09:19:33 +0200

    Fixed dismiss button and correct image of the appetizer when its clicked

--- Appetizers/Screens/AppetizerListView/AppetizerDetailView.swift
+++ Appetizers/Screens/AppetizerListView/AppetizerDetailView.swift
@@ -10,11 +10,11 @@ import SwiftUI
 struct AppetizerDetailView: View {
   
   let appetizer: Appetizer
+  @Binding var isShowingDetail: Bool
   
     var body: some View {
       VStack {
-        Image("asian-flank-steak")
-          .resizable()
+        AppetizerRemoteImage(urlString: appetizer.imageURL)
           .aspectRatio(contentMode: .fit)
           .frame(width: 300, height: 225)
         
@@ -83,7 +83,7 @@ struct AppetizerDetailView: View {
       .cornerRadius(12)
       .shadow(radius: 40)
       .overlay(Button {
-        print("dismiss")
+        isShowingDetail = false
       } label: {
         ZStack {
           Circle()
@@ -101,5 +101,6 @@ struct AppetizerDetailView: View {
 }
 
 #Preview {
-  AppetizerDetailView(appetizer: MockData.sampleAppetizer)
+  AppetizerDetailView(appetizer: MockData.sampleAppetizer, 
+                      isShowingDetail: .constant(true))
 }
--- Appetizers/Screens/AppetizerListView/AppetizerListView.swift
+++ Appetizers/Screens/AppetizerListView/AppetizerListView.swift
@@ -11,19 +11,33 @@ struct AppetizerListView: View {
   
   // listening to the changes in AppertizerListViewModel
   @StateObject var viewModel = AppetizerListViewModel()
+  @State private var isShowingDetail = false
+  @State private var selectedAppetizer: Appetizer?
   
   var body: some View {
     ZStack {
       NavigationView {
         List(viewModel.appetizers) { appetizer in
           AppetizerListCell(appetizer: appetizer)
+            .onTapGesture {
+              selectedAppetizer = appetizer
+              isShowingDetail = true
+            }
           
         }
         .navigationTitle("Appetizers")
+        .disabled(isShowingDetail)
       }
       .onAppear {
         viewModel.getAppetizers()
       }
+      .blur(radius: isShowingDetail ? 20 : 0)
+      
+      if isShowingDetail {
+        AppetizerDetailView(appetizer: selectedAppetizer!,
+                            isShowingDetail: $isShowingDetail)
+      }
+      
       if viewModel.isLoading {
         LoadingView()
       }
