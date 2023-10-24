3c7f2e5e0b85a0dc451ecf6dfe78200ab3ac357e
Vicgri <89450050+Vicgri@users.noreply.github.com>
2023-10-24 22:01:22 +0200

    OrderView UI, added MockData and Swipe to delete function

--- Appetizers/Model/Appetizer.swift
+++ Appetizers/Model/Appetizer.swift
@@ -26,9 +26,10 @@ struct AppetizerResponse: Decodable {
   
 }
 
+// Dummy data
 struct MockData {
   
-  static let sampleAppetizer = Appetizer(id: 0001,
+  static let sampleAppetizer      = Appetizer(id: 0001,
                                          name: "Test Appetizer",
                                          description: "This is the description for my appetizer",
                                          price: 9.99,
@@ -37,8 +38,37 @@ struct MockData {
                                          protein: 99,
                                          carbs: 99)
   
-  // array to help us build a list
-  static let appetizers = [sampleAppetizer, sampleAppetizer, sampleAppetizer, sampleAppetizer ]
+  // array to help build a list
+  static let appetizers           = [sampleAppetizer, sampleAppetizer, sampleAppetizer, sampleAppetizer ]
+  
+  static let orderItemOne         = Appetizer(id: 0001,
+                                         name: "Test Appetizer 1",
+                                         description: "This is the description for my appetizer",
+                                         price: 9.99,
+                                         imageURL: "",
+                                         calories: 99,
+                                         protein: 99,
+                                         carbs: 99)
+  
+  static let orderItemTwo         = Appetizer(id: 0002,
+                                              name: "Test Appetizer 2",
+                                              description: "This is the description for my appetizer",
+                                              price: 9.99,
+                                              imageURL: "",
+                                              calories: 99,
+                                              protein: 99,
+                                              carbs: 99)
+  
+  static let orderItemThree        = Appetizer(id: 0003,
+                                              name: "Test Appetizer 3",
+                                              description: "This is the description for my appetizer",
+                                              price: 9.99,
+                                              imageURL: "",
+                                              calories: 99,
+                                              protein: 99,
+                                              carbs: 99)
+  
+  static let orderItems           =  [orderItemOne, orderItemTwo, orderItemThree]
   
 }
 
--- Appetizers/Screens/OrderView/OrderView.swift
+++ Appetizers/Screens/OrderView/OrderView.swift
@@ -2,20 +2,43 @@
 //  OrderView.swift
 //  Appetizers
 //
-//  Created by Victoria Grimen on 10/10/2023.
+//  Created by Victoria Grimen on 24/10/2023.
 //
 
 import SwiftUI
 
 struct OrderView: View {
+  
+  @State private var orderItems = MockData.orderItems
+  
   var body: some View {
     NavigationView {
-      Text("Orders")
-        .navigationTitle("Orders")
+      VStack {
+        List {
+          ForEach(orderItems) { appetizer in
+            AppetizerListCell(appetizer: appetizer)
+          }
+          .onDelete(perform: deleteItems)
+        }
+        .listStyle(PlainListStyle())
+        
+        Button {
+          print("Order placed")
+        } label: {
+          ApButton(title: "$99.99 - Place Order")
+        }
+        .padding(.bottom, 25)
+      }
+      
+      .navigationTitle("Orders")
     }
   }
+  func deleteItems (at offesets: IndexSet) {
+    orderItems.remove(atOffsets: offesets)
+  }
+  
 }
 
 #Preview {
-    OrderView()
+  OrderView()
 }
