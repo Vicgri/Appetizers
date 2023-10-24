767c653cf7831f276bced7ef3a007c21877773f9
Vicgri <89450050+Vicgri@users.noreply.github.com>
2023-10-24 21:39:32 +0200

    Added AppStorage to User when information about the User is saved

--- Appetizers/Screens/AccountView/AccountView.swift
+++ Appetizers/Screens/AccountView/AccountView.swift
@@ -8,50 +8,52 @@
 import SwiftUI
 
 struct AccountView: View {
-
-@StateObject var viewModel = AccountViewModel()
-
-
-
-var body: some View {
-NavigationView {
-  Form {
-    Section(header: Text("Personal Info")) {
-      TextField("First Name", text: $viewModel.firstName)
-      TextField("Last Name", text: $viewModel.lastName)
-      TextField("Email", text: $viewModel.email)
-        .keyboardType(.emailAddress)
-        .autocapitalization(/*@START_MENU_TOKEN@*/.none/*@END_MENU_TOKEN@*/)
-        .disableAutocorrection(true)
-      
-      DatePicker("Birthday", selection: $viewModel.birthdate,
-                 displayedComponents: .date)
-      
-      Button {
-        viewModel.saveChanges()
-      } label: {
-        Text("Save Changes")
+  
+  @StateObject var viewModel = AccountViewModel()
+  
+  
+  var body: some View {
+    NavigationView {
+      Form {
+        Section(header: Text("Personal Info")) {
+          TextField("First Name", text: $viewModel.user.firstName)
+          TextField("Last Name", text: $viewModel.user.lastName)
+          TextField("Email", text: $viewModel.user.email)
+            .keyboardType(.emailAddress)
+            .autocapitalization(/*@START_MENU_TOKEN@*/.none/*@END_MENU_TOKEN@*/)
+            .disableAutocorrection(true)
+          
+          DatePicker("Birthday", selection: $viewModel.user.birthdate,
+                     displayedComponents: .date)
+          
+          Button {
+            viewModel.saveChanges()
+          } label: {
+            Text("Save Changes")
+          }
+        }
+        Section(header: Text("Requests")) {
+          Toggle("Extra Napkins", isOn: $viewModel.user.extraNapkins)
+          Toggle("Frequent Refills", isOn: $viewModel.user.frequentRefills)
+        }
+        .toggleStyle(SwitchToggleStyle(tint: .BrandPrimary))
+        
       }
+      .navigationTitle("Account")
+      
     }
-    Section(header: Text("Requests")) {
-      Toggle("Extra Napkins", isOn: $viewModel.extraNapkins)
-      Toggle("Frequent Refills", isOn: $viewModel.frequentRefills)
+    .onAppear(){
+      viewModel.retriveUser()
     }
-    .toggleStyle(SwitchToggleStyle(tint: .BrandPrimary))
-   
+    .alert(item: $viewModel.alertItem) {alertItem in
+      Alert(title: alertItem.title,
+            message: alertItem.message,
+            dismissButton: alertItem.dismissButton)
+    }
   }
-  .navigationTitle("Account")
-    
-  }
-.alert(item: $viewModel.alertItem) {alertItem in
-  Alert(title: alertItem.title,
-        message: alertItem.message,
-        dismissButton: alertItem.dismissButton)
-}
-}
 }
 
 
 #Preview {
-AccountView()
+  AccountView()
 }
